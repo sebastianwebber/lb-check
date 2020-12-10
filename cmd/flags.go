9b34bb581db4d97e2976894d391301f8101b3940
Sebastian Webber <sebastian@swebber.me>
2020-12-10 15:53:56 -0300

    feat: remove args sort
    also improved some help messages for the flags.
    
    Signed-off-by: Sebastian Webber <sebastian@swebber.me>

--- cmd/flags.go
+++ cmd/flags.go
@@ -35,6 +35,8 @@ func init() {
 		panic(err)
 	}
 
+	flag.CommandLine.SortFlags = false
+
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "%s checks if the PostgreSQL is running and up to date.\n\n", appName)
 		fmt.Fprintf(os.Stderr, "Usage:\n")
@@ -42,21 +44,22 @@ func init() {
 		fmt.Fprintf(os.Stderr, "General Options:\n")
 
 		flag.PrintDefaults()
+
 		fmt.Fprintf(os.Stderr, "\nReport BUGs on https://github.com/sebastianwebber/lb-check.\n\n")
 	}
 
-	flag.String("pg-isready-bin", "/usr/bin/pg_isready", "pg_isready binary path")
+	flag.String("pg-isready-bin", "/usr/bin/pg_isready", "pg_isready binary path\n")
 
 	flag.StringP("username", "U", user.Username, "database user name")
 	flag.StringP("host", "h", "local socket", "database server host or socket directory")
 	flag.StringP("dbname", "d", user.Username, "database name to connect to")
 	flag.IntP("port", "p", 5432, "database server port")
-	flag.BoolP("password", "W", false, "force password prompt")
+	flag.BoolP("password", "W", false, "to force password prompt.Use PGPASSWORD to the define the password.\n")
 
 	flag.String("query-delay-seconds", "SELECT CASE WHEN pg_last_wal_receive_lsn() = pg_last_wal_replay_lsn() THEN 0 ELSE  EXTRACT(EPOCH FROM (now() - pg_last_xact_replay_timestamp()))::INTEGER END AS delay;", "query to check the replica delay in bytes")
-	flag.String("query-recovery", "SELECT pg_is_in_recovery() as recovery;", "query to check if the replica is in recovery mode")
+	flag.String("query-recovery", "SELECT pg_is_in_recovery() as recovery;", "query to check if the replica is in recovery mode\n")
 
-	flag.Duration("max-delay", 60*time.Second, "max delay allowed to a replica")
+	flag.Duration("max-delay", 60*time.Second, "max delay allowed to a replica\n")
 
 	var checkOnlyHelp bytes.Buffer
 
